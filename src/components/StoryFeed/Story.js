fdeb28c29abaecb6d3f7df4d7b4b126d016c6b4f
Dave Vazquez <dmvaz.li@gmail.com>
2020-09-26 17:18:42 -0400

    removes comments

--- src/components/StoryFeed/Story.js
+++ src/components/StoryFeed/Story.js
@@ -26,33 +26,6 @@ const Story = ({ story }) => {
       </p>
     </div>
   );
-
-  // return (
-  //   <article id="story">
-  //     <img id="upvote" src={upvote} alt="upvote article arrow" />
-  //     <header>
-  //       <h2>
-  //         <a
-  //           href={story.url}
-  //           rel="noopener noreferrer"
-  //           target="_blank"
-  //         >
-  //           {story.title}
-  //           <span id="source-url"> (https://somehowmanage.com)</span>
-  //         </a>
-  //       </h2>
-  //     </header>
-  //     <p id="details">
-  //       <span>{story.score} points </span>
-  //       <span>by {story.by} </span>
-  //       <time>{timeElapsed} | </time>
-  //       <span>hide | </span>
-  //       <Link to={`/story/${story.id}`}>
-  //         {story.descendants} comments
-  //       </Link>
-  //     </p>
-  //   </article>
-  // );
 };
 
 export default Story;
