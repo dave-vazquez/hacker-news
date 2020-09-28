fcd6c8e3215326b5281138e66d1d132d4498138b
Dave Vazquez <dmvaz.li@gmail.com>
2020-09-27 20:32:03 -0400

    changes y-combinator logo image type

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,14 @@ import "./header.scss";
 
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import y18 from "../../assets/y18.gif";
+import hackernews from "../../assets/hackernews-logo.png";
 
 const Header = () => {
   return (
     <header id="main">
       <nav aria-label="site-wide">
         <Link to="/">
-          <img id="hacker-news-logo" src={y18} alt="hacker news logo" />
+          <img id="hacker-news-logo" src={hackernews} alt="hacker news logo" />
         </Link>
         <div>
           <h1><Link to="/stories/top/page/0" id="hacker-news"> Hacker News </Link></h1>
